d18befcd464814c103580766cb9366920ab31918
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-27 13:27:57 -0400

    Commented out the vue component

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <contacts-component :contacts-data='{{ $contacts->toJSON() }}'></contacts-component>
+    <!-- <contacts-component></contacts-component> -->
 @endsection
