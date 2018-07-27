c0e6527c2f62c9750cbfb8a6ec14843979de08b0
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-27 10:03:52 -0400

    Fixes routes between welcome page and home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $contacts = \App\Contact::where('user_id', '=', auth::user())->get();
+
+        return view('home', compact('contacts'));
     }
 }
