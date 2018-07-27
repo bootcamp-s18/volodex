53bb0af1fa53c52005f9376f518320660b326cda
Chris Browder <rcbrowder@gmail.com>
2018-07-27 16:07:38 -0400

    ...

---
+++ resources/views/vcards/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ $vcard }}</h3>
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    <!-- <contacts-component :vcards-data="{{ $vcards->toJSON() }}"></contacts-component> -->
+@endsection
