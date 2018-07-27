e0143c4acf3e9b9b0bb4a317e25cba5b82cc27e9
Chris Browder <rcbrowder@gmail.com>
2018-07-27 15:19:23 -0400

    Small edit...

--- resources/views/vcards/edit.blade.php
+++ resources/views/vcards/edit.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    
 
+    <form method="post" action="/vcards/{{ $vcard->id }}">
+        @csrf
+		@method('PUT')
+
+        <div class="form-group">
+			<label for="name_first">First Name:</label>
+			<input type="text" class="form-control" name="name_first" id="name" value="{{ $vcard->name }}">
+		</div>
+
+        
+
+
+    </form>
     <!-- <contacts-component :vcards-data="{{ $vcards->toJSON() }}"></contacts-component> -->
 
 
